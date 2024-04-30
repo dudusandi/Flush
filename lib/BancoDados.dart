5ef6d739aec6ee0ef4a9ef70fcb4857b35ab12f4
Duda <dudaesouza@gmail.com>
2024-04-30 04:47:00 -0300

    .

--- lib/BancoDados.dart
+++ lib/BancoDados.dart
@@ -1,114 +1,92 @@
 import 'package:postgres/postgres.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-
-class Banco{
-
+class Banco {
   var _host;
+  var _database;
+  var _usuario;
+  var _senha;
+  var _selectedItem;
+  var _ssl;
+  var _port;
+  var _portaController;
 
   get host => _host;
 
   set host(value) {
     _host = value;
   }
-  var _database;
 
   get database => _database;
-
   set database(value) {
     _database = value;
   }
-  var _usuario;
 
   get usuario => _usuario;
-
   set usuario(value) {
     _usuario = value;
   }
-  var _senha;
 
   get senha => _senha;
-
   set senha(value) {
     _senha = value;
   }
 
-  var _selectedItem;
-
   get selectedItem => _selectedItem;
-
   set selectedItem(value) {
     _selectedItem = value;
   }
 
-  var _ssl;
-
   get ssl => _ssl;
-
   set ssl(value) {
     _ssl = value;
   }
 
-  var _port;
-
   get port => _port;
-
   set port(value) {
     _port = value;
   }
 
-  var _portaController;
-
   get portaController => _portaController;
-
   set portaController(value) {
     _portaController = value;
   }
 
   Future<Connection> conectarbanco() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
 
-      SharedPreferences prefs = await SharedPreferences.getInstance();
+    _host = prefs.getString('host') ?? '';
+    _portaController = prefs.getString('porta') ?? '';
+    _database = prefs.getString('database') ?? '';
+    _usuario = prefs.getString('usuario') ?? '';
+    _senha = prefs.getString('senha') ?? '';
+    _selectedItem = prefs.getString('itemSelecionado');
 
-      _host = prefs.getString('host') ?? '';
-      _portaController = prefs.getString('porta') ?? '';
-      _database = prefs.getString('database') ?? '';
-      _usuario = prefs.getString('usuario') ?? '';
-      _senha = prefs.getString('senha') ?? '';
-      _selectedItem = prefs.getString('itemSelecionado');
+    SslMode sslMode() {
+      return _selectedItem == 'ativado' ? SslMode.require : SslMode.disable;
+    }
 
-      SslMode sslMode() {
-        return _selectedItem == 'ativado' ? SslMode.require : SslMode.disable;
-      }
-
-
-      try {
-        final conn = await Connection.open(Endpoint(
-
+    try {
+      final conn = await Connection.open(
+        Endpoint(
             host: _host,
             database: _database,
             username: _usuario,
             password: _senha,
-            port: int.parse(_portaController)
+            port: int.parse(_portaController)),
+        settings: ConnectionSettings(sslMode: sslMode()),
+      );
 
-        ),
-
-          settings: ConnectionSettings(sslMode: sslMode()),
-        );
-
-        return conn;
-      }catch (e){
-        return Future.error(e);
-
-      }
+      return conn;
+    } catch (e) {
+      return Future.error(e);
+    }
   }
 
   Future<void> criarbanco() async {
+    Connection conn = await conectarbanco();
 
-      Connection conn = await conectarbanco();
-
-      await conn.execute(
-          '''
+    await conn.execute('''
       CREATE TABLE IF NOT EXISTS public.pesquisadores(
     nome text COLLATE pg_catalog."default" NOT NULL,
     cpf text COLLATE pg_catalog."default" NOT NULL,
@@ -116,25 +94,20 @@ class Banco{
     areaconhecimento text COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT pesquisadores_pkey PRIMARY KEY (nome)
 )
-     '''
-      );
-      await conn.close();
-
+     ''');
+    await conn.close();
   }
 
   Future<void> adicionarPesquisador(
       String nome, String cpf, String tipo, String area) async {
-
     Connection conn = await conectarbanco();
 
     await conn.execute('''
       INSERT INTO pesquisadores (nome, cpf, tipo, areaconhecimento) 
       VALUES ('$nome', '$cpf', '$tipo', '$area')
-      '''
-    );
+      ''');
     await conn.close();
   }
-
 
   Future<void> removerPesquisador(String nome) async {
     Connection conn = await conectarbanco();
@@ -142,9 +115,7 @@ class Banco{
     await conn.execute('''
       DELETE FROM pesquisadores 
       WHERE nome = '$nome'
-      '''
-    );
+      ''');
     await conn.close();
   }
-
 }
