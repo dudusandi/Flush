bffe8139d579a7ae2f279411df3d438e961088ca
alice <anovello4@ucs.br>
2024-05-14 21:42:11 -0300

    t

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,26 +6,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image(
+    return Center(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: Image(
             image: AssetImage('assets/logoucs.png'),
-            width: 200,
-            height: 200,
+            width: 100,
+            height: 100,
             filterQuality: FilterQuality.high,
             isAntiAlias: true,
           ),
-          Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Text(
-              "Sistema de Pesquisas e Inovação",
-            ),
-          )
-        ],
-      ),
-    );
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 20, right: 5, left: 10),
+          child: Text(
+            textAlign: TextAlign.justify,
+            "Seja bem vindo ao Flush, o sistema de Pesquisas e Inovação da UCS. Aqui você poderá cadastrar os pesquisadores da institução, seus respectivos projetos e conectá-los a empresas investidoras.",
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: ElevatedButton(
+            child: Text('Cadastrar Pesquisador'),
+            onPressed: () {
+              Navigator.pushNamed(context, '/cadastro');
+            },
+          ),
+        ),
+        Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: ElevatedButton(
+                child: Text('Cadastrar Projeto'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/cadastro_projeto');
+                })),
+      ],
+    ));
   }
 }
